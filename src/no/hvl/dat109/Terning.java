f8853d0c83a8702471bd23a33f22e35c1021fb06
S571736 <Sondre.Gjesdal@gmail.com>
2019-01-22 14:09:20 +0100

    Animals are hopefully tamed, let's hope we won't roll a untamed lion...

--- src/no/hvl/dat109/Terning.java
+++ src/no/hvl/dat109/Terning.java
@@ -4,15 +4,33 @@ import java.util.Random;
 
 public class Terning {
 
+    static Dyr currentDyr;
+
+    public static void setCurrentDyr(Dyr currentDyr) {
+        Terning.currentDyr = currentDyr;
+    }
+
+    public Dyr trill() {
+        setCurrentDyr(Dyr.getTilfeldigDyr());
+        return currentDyr;
+    }
 
     private enum Dyr {
-        LØVE, SLANGE, PANDA, GRIS, ELEFANT, HVAL;
+        LØVE,
+        SLANGE,
+        PANDA,
+        GRIS,
+        ELEFANT,
+        HVAL;
 
-
-        public static Dyr getTilfeldigDyr {
+        /**
+         * Velger en tilfeldig verdi fra Dyr enum'en.
+         *
+         * @return et tilfeldig dyr.
+         */
+        public static Dyr getTilfeldigDyr() {
             Random random = new Random();
             return values()[random.nextInt(values().length)];
         }
     }
-
 }

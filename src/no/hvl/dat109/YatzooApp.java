431a854fb9b3c47cbdb6ac6e50a62f677608626f
Enah <182255@stud.hvl.no>
2019-01-29 11:53:33 +0100

    antar at sånn appen skal initieres

--- src/no/hvl/dat109/YatzooApp.java
+++ src/no/hvl/dat109/YatzooApp.java
@@ -6,7 +6,10 @@ package no.hvl.dat109;
 public class YatzooApp{
     public static void main(String[] args){
         YatzooSpill yspill = new YatzooSpill();
+
         yspill.spill();
+
+        System.out.println("Vinneren er " + yspill.finnVinner());
 
     }
 

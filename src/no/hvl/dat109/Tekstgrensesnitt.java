1eaa8de416321cd9d24320724409dadaf3949b61
571530 <truls.martinsen13@gmail.com>
2019-01-29 11:16:09 +0100

    Endringer i klassediagrammet og start på tekstgrensesnitt klassen

--- src/no/hvl/dat109/Tekstgrensesnitt.java
+++ src/no/hvl/dat109/Tekstgrensesnitt.java
@@ -1,15 +1,12 @@
 package no.hvl.dat109;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tekstgrensesnitt {
-    private YatzooSpill yatzoo;
-
-    public Tekstgrensesnitt() {
-        yatzoo = new YatzooSpill();
-    }
-
-    public void init() {
+    public List<Spiller> lesSpillere() {
+        ArrayList<Spiller> spillere = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Antall spillere?");
@@ -18,8 +15,10 @@ public class Tekstgrensesnitt {
         for (int i = 0; i < antallSpillere; i++) {
             System.out.println("Navn på spiller nr " + i + "?");
             String navn = sc.nextLine();
-            yatzoo.leggTilSpiller(new Spiller(navn));
+            spillere.add(new Spiller(navn));
         }
+
+        return spillere;
     }
 
 }
